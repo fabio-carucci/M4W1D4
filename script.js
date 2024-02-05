eb67ae19d0f436cd77fc77d69fcf87bfeba8ab18
Fabio Carucci <garu2000@live.it>
2024-02-05 18:36:51 +0100

    Funzione che legge i titoli dell'album (EXTRA) fatta

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// Funzione per restituire a console i titoli degli album
+function leggiTitoliAlbum(json) {
+    let songs = json.data;
+    console.log(songs[0].artist.name + ":");
+    for (let i = 0; i < songs.length; i++) {
+        let song = songs[i];
+        console.log(`${i+1} ${song.album.title}`);
+    }
+}
 
+
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+    .then((Response) => Response.json())
+    .then(leggiTitoliAlbum)
+    .catch((err) => console.log("Request failed", err));
+
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica")
+    .then((Response) => Response.json())
+    .then(leggiTitoliAlbum)
+    .catch((err) => console.log("Request failed", err));
+
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen")
+    .then((Response) => Response.json())
+    .then(leggiTitoliAlbum)
+    .catch((err) => console.log("Request failed", err));
